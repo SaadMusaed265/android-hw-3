ca1b16c404cd1e9053c9b5f3736fe02e9e66f2e9
saadMusaed265 <zsalmanx76@gamil.com>
2020-06-24 07:26:31 +0300

    Update MainActivity.java

--- saad 265/knafa2/app/src/main/java/com/example/knafa/MainActivity.java
+++ saad 265/knafa2/app/src/main/java/com/example/knafa/MainActivity.java
@@ -13,7 +13,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button knafa = findViewById(R.id.nextPage);
+        Button knafa = findViewById(R.id.nextpage);
         knafa.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
